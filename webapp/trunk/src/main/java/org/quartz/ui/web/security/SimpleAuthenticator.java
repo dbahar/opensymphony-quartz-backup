176650fadf5500bb06b18a69ac9a1fe562ca2ae0
matthew.payne <matthew.payne@1a27cac7-c2d7-d94c-91fd-b96639b1a720>
2008-10-20 21:11:53 +0000

    update rev on jetty

--- webapp/trunk/src/main/java/org/quartz/ui/web/security/SimpleAuthenticator.java
+++ webapp/trunk/src/main/java/org/quartz/ui/web/security/SimpleAuthenticator.java
@@ -10,89 +10,89 @@ package org.quartz.ui.web.security;
  */
 public class SimpleAuthenticator implements Authenticator {
 
-    Users users;
+	Users users;
 
-    public SimpleAuthenticator() {
-
-    }
-
-    private String username = "";
-    private String password = "";
-
-    /**
-     * @return Returns the users.
-     */
-    public Users getUsers() {
-	return users;
-    }
-
-    /**
-     * @param users
-     *                The users to set.
-     */
-    public void setUsers(Users users) {
-	this.users = users;
-    }
-
-    /**
-     * @return Returns the password.
-     */
-    public String getPassword() {
-	return password;
-    }
-
-    /**
-     * @param password
-     *                The password to set.
-     */
-    public void setPassword(String password) {
-	this.password = password;
-    }
-
-    /**
-     * @return Returns the username.
-     */
-    public String getUsername() {
-	return username;
-    }
-
-    /**
-     * @param username
-     *                The username to set.
-     */
-    public void setUsername(String username) {
-	this.username = username;
-    }
-
-    /**
-     * Authenticate a user.
-     * 
-     * Implement this method in a subclass to avoid dealing with Principal
-     * objects.
-     * 
-     * @param username
-     *                a username
-     * @param password
-     *                a plain text password, as entered by the user
-     * 
-     * @return null if the user cannot be authenticated, otherwise a Pricipal
-     *         object is returned
-     */
-
-    public AuthenticationResult authenticate(UserDetails userDetails) throws Exception {
-	AuthenticationResult result = null;
-
-	if (this.username.equals(username) && this.password.equals(password)) {
-	    result = new AuthenticationResult(true, SUCCESS, new User(username, password));
-	    ;
-	} else {
-
-	    result = new AuthenticationResult(false, FAIL, null);
+	public SimpleAuthenticator() {
 
 	}
 
-	return result;
-    }
+	private String username = "";
+	private String password = "";
+
+	/**
+	 * @return Returns the users.
+	 */
+	public Users getUsers() {
+		return users;
+	}
+
+	/**
+	 * @param users
+	 *            The users to set.
+	 */
+	public void setUsers(Users users) {
+		this.users = users;
+	}
+
+	/**
+	 * @return Returns the password.
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password
+	 *            The password to set.
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return Returns the username.
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username
+	 *            The username to set.
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * Authenticate a user.
+	 * 
+	 * Implement this method in a subclass to avoid dealing with Principal
+	 * objects.
+	 * 
+	 * @param username
+	 *            a username
+	 * @param password
+	 *            a plain text password, as entered by the user
+	 * 
+	 * @return null if the user cannot be authenticated, otherwise a Pricipal
+	 *         object is returned
+	 */
+
+	public AuthenticationResult authenticate(UserDetails userDetails) throws Exception {
+		AuthenticationResult result = null;
+
+		if (username.equals(username) && password.equals(password)) {
+			result = new AuthenticationResult(true, SUCCESS, new User(username, password));
+			;
+		} else {
+
+			result = new AuthenticationResult(false, FAIL, null);
+
+		}
+
+		return result;
+	}
 }
 
 // ----------------------------------------------------------------------------
